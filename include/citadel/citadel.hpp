13a4a6746fb8e2df00319fcd2850b07073fc5e14
Nessbe <felix.chaupitre@gmail.com>
2025-11-20 21:18:05 +0100

    Filesystem: Integrated file open mode enumeration to control read/write access

--- include/citadel/citadel.hpp
+++ include/citadel/citadel.hpp
@@ -46,6 +46,7 @@
 #include "citadel/drivers/opengl/opengl_surface.hpp"
 
 #include "citadel/io/filesystem/file.hpp"
+#include "citadel/io/filesystem/file_open_mode.hpp"
 
 #include "citadel/math/angles.hpp"
 
