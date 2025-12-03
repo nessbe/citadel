b8115a145cdec2e9e852247f39c50f709f9d67eb
Nessbe <felix.chaupitre@gmail.com>
2025-12-03 19:00:19 +0100

    OpenGL: Enabled OpenGL blending

--- source/drivers/opengl/opengl_context.cpp
+++ source/drivers/opengl/opengl_context.cpp
@@ -103,6 +103,9 @@ namespace citadel {
 #endif
 
 		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_BLEND);
+
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
 
 	opengl_context::~opengl_context() {
